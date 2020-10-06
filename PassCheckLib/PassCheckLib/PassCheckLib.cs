70be11fa15157d76f4216fa40a65409d610be65b
AlexeyMikhylov <56645895+AlexeyMikhylov@users.noreply.github.com>
2020-10-06 16:28:47 +0300

    password validation library

---
+++ PassCheckLib/PassCheckLib/PassCheckLib.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PassCheckLib
+{
+    public class PasswordCheckClass
+    {
+        public string passLengthCheck(string password)
+        {
+            string result;
+
+            if (password.Length >= 8)
+            {
+                Console.WriteLine("Достаточная длина пароля");
+                return result = "ok";
+            }
+            else
+            {
+                Console.WriteLine("Не достаточная длина пароля");
+                return result = "not ok";
+            }
+        }
+
+        public string passCheck(string password)
+        {
+            Regex regex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,32}$");
+            string result;
+
+            if (regex.IsMatch(password))
+            {
+                Console.WriteLine("Надежный пароль");
+                return result = "Надежный пароль";
+            }
+            else
+            {
+                Console.WriteLine("ненадежный пароль");
+                return result = "ненадежный пароль";
+            }
+        }
+    }
+}
---
+++ PassCheckLib/PassCheckLibUnitTest/UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PassCheckLib;
+
+namespace PassCheckLibUnitTest
+{
+    [TestClass]
+    public class PassCheckTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            string password = "PassWord123";
+            string expected = "ok";
+            //act
+            PasswordCheckClass passch = new PasswordCheckClass();
+            string actual = passch.passLengthCheck(password);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //arrange
+            string password = "Pass1";
+            string expected = "not ok";
+            //act
+            PasswordCheckClass passch = new PasswordCheckClass();
+            string actual = passch.passLengthCheck(password);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //arrange
+            string password = "password123"; //no uppercase
+            string expected = "ненадежный пароль";
+            //act
+            PasswordCheckClass passch = new PasswordCheckClass();
+            string actual = passch.passCheck(password);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //arrange
+            string password = "PASSWORD123"; //no lowercase
+            string expected = "ненадежный пароль";
+            //act
+            PasswordCheckClass passch = new PasswordCheckClass();
+            string actual = passch.passCheck(password);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //arrange
+            string password = "PassWord"; //no numbers
+            string expected = "ненадежный пароль";
+            //act
+            PasswordCheckClass passch = new PasswordCheckClass();
+            string actual = passch.passCheck(password);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //arrange
+            string password = "PassWord123"; //all yes
+            string expected = "Надежный пароль";
+            //act
+            PasswordCheckClass passch = new PasswordCheckClass();
+            string actual = passch.passCheck(password);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
